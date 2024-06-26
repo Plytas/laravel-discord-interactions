ea73a1ddcf995667912d5a9b7680d7e11f2c284e
Plytas <Plytas@users.noreply.github.com>
2024-06-26 14:28:33 +0000

    Fix styling

--- src/Data/DiscordGuild.php
+++ src/Data/DiscordGuild.php
@@ -6,11 +6,9 @@ use Spatie\LaravelData\Data;
 
 class DiscordGuild extends Data
 {
-	public function __construct(
-		public string $id,
+    public function __construct(
+        public string $id,
         public string $name,
         public ?string $icon = null,
-	)
-	{
-	}
+    ) {}
 }
